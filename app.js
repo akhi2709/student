b1dd9fc71418d01a79926866593ec2490a42c851
A <akhileshshinde2709@gmail.com>
2025-03-25 02:41:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("./database");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
+
+// MongoDB Connection
+const MONGO_URI = "your_mongodb_connection_string_here";
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected successfully"))
+.catch(err => console.log("MongoDB connection error:", err));
+
 app.use(bodyParser.json());
 app.use("/api/users", routes);
 
